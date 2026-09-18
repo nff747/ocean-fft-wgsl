885a6f7302565309d10ef28d98bd256b4402a70c
nff747 <nff747@users.noreply.github.com>
2026-09-18 14:30:00 -0300

    feat: add public library entrypoint and exports for Ocean FFT engine

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+/**
+ * Ocean FFT WGSL
+ * Real-Time Phillips Spectrum & Tessendorf Ocean Wave FFT Simulation in WebGPU / WGSL
+ * @packageDocumentation
+ */
+
+export * from './types';
+export * from './math/spectrum';
+export * from './math/fft';
+export * from './core/OceanSimulator';
+export * from './core/CPUReferenceOcean';
+export * from './core/ThreeOceanMesh';
+
+export { oceanSpectrumShader } from './shaders/oceanSpectrum.wgsl';
+export { oceanFftShader } from './shaders/oceanFft.wgsl';
+export { oceanNormalsShader } from './shaders/oceanNormals.wgsl';
